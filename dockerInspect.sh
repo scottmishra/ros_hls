8b529a4babc18fc5b0a75f0785166050a1aba822
Scott Mishra <scott.mishra@parivedasolutions.com>
2017-09-21 10:34:21 -0500

    minor changes to inspect to see the name for each ip

--- dockerInspect.sh
+++ dockerInspect.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # display the host ip of each of the docker containers
-
+echo "listener:"
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' listener
-
+echo "talker:"
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' talker
-
+echo "master:"
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' master
